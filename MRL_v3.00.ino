6e731b2f2dcb041053a85c1f5029891163f5f715
Ozgun <41177296+ozguney@users.noreply.github.com>
2021-11-15 10:51:12 +0300

    Add files via upload

---
+++ MRL_v3.00.ino
@@ -0,0 +1,36 @@
+#include <Adafruit_NeoPixel.h> // Add Led Library
+#define PIN 6
+#define NUM_LEDS 88
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+#define RedDensity    150
+#define GreenDensity  199
+#define BlueDensity   88
+byte note; // make available for mathematical operation
+
+void OnNoteOn(byte channel, byte note, byte velocity) {
+    strip.setPixelColor(note-20, RedDensity, GreenDensity, BlueDensity);
+    strip.show();
+    digitalWrite(LED_BUILTIN, HIGH); // For feedback
+}
+
+void OnNoteOff(byte channel, byte note, byte velocity) {
+    strip.setPixelColor(note-20, 0, 0, 0);
+    strip.show();
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW); // Blink LED Once
+  
+  strip.begin(); // start the strip and blank it out
+  strip.show();
+  usbMIDI.setHandleNoteOff(OnNoteOff);
+  usbMIDI.setHandleNoteOn(OnNoteOn);
+}
+
+void loop() {
+    usbMIDI.read();
+}
